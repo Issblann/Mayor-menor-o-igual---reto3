087871abc0fd73ca948a704afd05477a9467f448
issblann <anaisabelpp@outlook.com>
2022-11-30 14:31:02 -0500

    agrego reto 3 de mayor, menor o igual.

---
+++ main.js
@@ -0,0 +1,23 @@
+const numero1 = document.querySelector("#numero-1");
+const numero2 = document.querySelector("#numero-2");
+const parrafo = document.querySelector("#parrafo");
+const boton = document.getElementById("boton");
+
+// let text = "";
+const resultado = (text) => {
+  const valor1 = numero1.value;
+  const valor2 = numero2.value;
+  if (isNaN(valor1) || isNaN(valor2)) {
+    text = "Es necesario introducir dos numeros validos";
+  } else if (valor1 > valor2) {
+    text = ` ${valor1} es mayor que ${valor2}`;
+  } else if (valor1 < valor2) {
+    text = ` ${valor1} es menor que ${valor2}`;
+  } else if (valor1 === valor2) {
+    text = ` ${valor1} es igual a ${valor2}`;
+  }
+
+  parrafo.innerHTML = text;
+};
+
+boton.addEventListener("click", resultado);
